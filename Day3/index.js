0157b98e4cf9acc77898228445753018a21a2efd
JoaquinCostamagna <jncostamagna@gmail.com>
2023-12-18 16:15:33 -0300

    Day3

--- Day3/index.js
+++ Day3/index.js
@@ -23,6 +23,48 @@ function hasAdyacentSymbol(lines, lineNumber, number, index){
     return hasASymbol(previous) || hasASymbol(next) || hasASymbol(top) || hasASymbol(bottom);
 }
 
+function calculateGearRatios(lines, line, lineNumber) {
+    const lineRatios = [];
+
+    line.split('').forEach((element, index) => {
+        if (element !== '*') return;
+        else lineRatios.push(calculateGearRatio(lines, lineNumber, index));
+    });
+
+    return lineRatios;
+}   
+
+function calculateGearRatio(lines, lineNumber, index) {
+    // const top = lineNumber > 0 ? lines[lineNumber - 1].substring(index-1, index + 1) : null;
+    // const bottom = lineNumber < lines.length - 1 ? lines[lineNumber + 1].substring(index-1, index + 1) : null;
+
+    // const left = index > 0 ? lines[lineNumber][index - 1] : null;
+    // const right = index + 1 < lines[lineNumber].length? lines[lineNumber][index + 1] : null;
+
+    const top = exploreNumbers(lines, lineNumber - 1, index - 1, index + 1);
+    const bottom = exploreNumbers(lines, lineNumber + 1, index - 1, index + 1);
+    const left = exploreNumbers(lines, lineNumber, index - 1, index - 1);
+    const right = exploreNumbers(lines, lineNumber, index + 1, index + 1);
+    
+    const ratios = [...top, ...bottom, ...left, ...right];
+    
+    if (ratios.length === 2) return ratios[0] * ratios[1];
+    
+    return 0;
+}
+
+function exploreNumbers(lines, lineNumber, indexFrom, indexTo) {
+    const line = lines[lineNumber];
+    let match;
+    const lineNumbers = [];
+    while ((match = numberRegex.exec(line)) != null) {
+        const matchLength = match[0].length - 1;
+        if (match.index > indexTo || match.index + matchLength < indexFrom ) continue;
+        lineNumbers.push(parseInt(match[0]));
+    }
+    return lineNumbers;
+}
+
 function partOne() {
     const lines = fs.readFileSync('./input.txt', 'utf8').trim().split('\n');
 
@@ -49,4 +91,19 @@ function partOne() {
     console.log(result);
 }
 
-partOne();
+function partTwo() {
+    const lines = fs.readFileSync('./input.txt', 'utf8').trim().split('\n');
+    
+    const ratios = lines.map((line, lineNumber) => {
+        return calculateGearRatios(lines, line, lineNumber);
+    }).flat();
+
+    console.log(ratios);
+
+    const result = ratios.reduce((acc, number) => acc + number, 0);
+
+    console.log(result);
+}
+
+// partOne();
+partTwo();
